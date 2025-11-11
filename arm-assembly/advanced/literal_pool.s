837e63bd294485ffbea36727c3d9ce3bd3459707
JBurns <jzburns@gmail.com>
2025-11-11 14:50:43 +0400

    updates

---
+++ arm-assembly/advanced/literal_pool.s
@@ -0,0 +1,16 @@
+global_start
+
+_start:
+	ldr r0, =array	// trigger literal pool creation
+	str r2, [r0, #4]
+	ldr r7, =0x00FFFFAA
+	add r5, r6, r7
+	add r5, r6, r7
+	ldr r1, =array	// does this add another element to the pool?
+
+labelx:
+	add r5, r6, r7
+	.ltorg					// force the placement of the literal pool here
+
+.data
+	array: .word 1,2,3,4,5,6
