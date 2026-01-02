b2fbde94debc9e9bf90dc1bd4cc4e833477e2140
nakbarova <nakbarova11780@ada.edu.az>
2026-01-02 15:46:58 +0400

    Add hw-2.s (ARM reconstruction)

---
+++ homework/2/hw-2.s
@@ -0,0 +1,23 @@
+.global _start
+.text
+
+_start:
+    mov     r1, #0
+    mov     r2, #10
+    mov     r3, #0
+    mov     r4, #5
+
+loop:
+    subs    r5, r3, r4
+    addlt   r0, r0, r2
+    addlt   r3, r3, #1
+    blt     loop
+
+    bl      .+4
+    str     lr, [sp, #-4]!
+
+    mov     r4, #15
+    mov     r5, #10
+    add     r6, r5, r4
+    subs    r5, r3, r4
+    subs    r5, r3, r4
