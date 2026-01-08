7378393b23aade97e0d8d69ee785a75d0057a063
nakbarova <nakbarova11780@ada.edu.az>
2026-01-08 10:49:42 +0400

    Update hw-2.s

--- homework/2/hw-2.s
+++ homework/2/hw-2.s
@@ -2,22 +2,24 @@
 .text
 
 _start:
-    mov     r1, #0
-    mov     r2, #10
-    mov     r3, #0
-    mov     r4, #5
+    mov     r1, #0         @ e3a01000
+    mov     r2, #10        @ e3a0200a
+    mov     r3, #0         @ e3a03000
+    mov     r4, #5         @ e3a04005
 
 loop:
-    subs    r5, r3, r4
-    addlt   r0, r0, r2
-    addlt   r3, r3, #1
-    blt     loop
+    subs    r5, r3, r4     @ e0535004
+    addlt   r0, r0, r2     @ b0800002
+    addlt   r3, r3, #1     @ b2833001
+    blt     loop            @ bafffffb
 
-    bl      .+4
-    str     lr, [sp, #-4]!
+    bl      .+4            @ ebffffff
 
-    mov     r4, #15
-    mov     r5, #10
-    add     r6, r5, r4
-    subs    r5, r3, r4
-    subs    r5, r3, r4
+after_push:
+    str     lr, [sp, #-4]! @ e52de004
+
+    mov     r4, #15         @ e3a0400f
+    mov     r5, #10         @ e3a0500a
+    add     r6, r5, r4      @ e0856004
+    subs    r5, r3, r4      @ e0535004
+    b       after_push      @ eafffff9
